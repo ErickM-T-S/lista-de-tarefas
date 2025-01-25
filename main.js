ab4eabe4bd2165813e70ebea10b4ef22e9fd434e
ErickM-T-S <165426951+ErickM-T-S@users.noreply.github.com>
2025-01-25 16:03:43 -0300

    js

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 let tarefas = document.getElementById("inserir-tarefa")
 let lista = document.querySelector("div.lista")
-
+let marcado = false
 function Adicionar(){
     if(tarefas.value !='' ){
         let tarefa = document.createElement("p")
-        let excluir = document.createElement("p")
-        excluir.innerHTML = ' &#x2715;'
-        lista.appendChild(tarefa, excluir)
-        tarefa.innerHTML = '-'+ tarefas.value + excluir.innerHTML
+        lista.appendChild(tarefa,)
+        tarefa.innerHTML = '- '+ tarefas.value
         tarefas.value=''
-        excluir.classList
+       
+        tarefa.addEventListener('click', function(){
+            if(marcado ==false){
+                tarefa.style.textDecoration= 'line-through'
+                marcado = true
+            }else{
+                tarefa.style.textDecoration= 'none'
+                marcado = false
+            }
+        })
+        tarefa.addEventListener('dblclick', function(){
+            lista.removeChild(tarefa)
+        })
     
     }
     else{
         alert('adicione algo na lista ')
     }
+    
 }
+ 
