c3cf62de10fe93cd41ca79febc1576272312b009
ErickM-T-S <erickmatheussantossilva89@gmail.com>
2025-06-20 17:12:36 -0300

    main

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,74 @@
-let tarefas = document.getElementById("inserir-tarefa")
+let inserirTarefa = document.getElementById("inserir-tarefa")
 let erro = document.getElementById('erro')
-let lista = document.querySelector("div.lista")
-let marcado = false
-function Adicionar(){
-    if(tarefas.value !='' ){ 
-        let tarefa = document.createElement("p")
-        lista.appendChild(tarefa)
-        tarefa.innerHTML = '- '+ tarefas.value
-        tarefas.value=''
+let tarefas = []
+let botaoPrincipal = document.getElementById('botao')
+botaoPrincipal.onclick = () => adicionar()
+
+function adicionar(){
+    if(inserirTarefa.value.trim() != '' ){ 
+
+        tarefas.push(inserirTarefa.value)
+        renderizarTarefa()
         erro.innerHTML =''
-       
-        tarefa.addEventListener('click', function(){
-            if(marcado == false){
-                tarefa.style.textDecoration= 'line-through'
-                marcado = true
-            }else{
-                tarefa.style.textDecoration= 'none'
-                marcado = false
-            }
-        })
-        tarefa.addEventListener('dblclick', function(){
-            lista.removeChild(tarefa)
-        })
     
     }
     else{
         erro.innerHTML ='<strong> adicione algo na lista </strong>'
     }
+    inserirTarefa.value=''
+     
+}
+function renderizarTarefa(){
+        let lista = document.querySelector("ul.lista")
+        lista.innerHTML =''
+
+        for(let i =0; i< tarefas.length; i++){
+
+            let tarefa = document.createElement("li")
+            tarefa.textContent = '- '+ tarefas[i]
+
+            let botaoRemover = document.createElement('button')
+            botaoRemover.className ='remover'
+            botaoRemover.textContent ='remover'
+            botaoRemover.onclick = () => remover(i)
+
+            let botaoEditar = document.createElement('button')
+            botaoEditar.className ='editar'
+            botaoEditar.textContent ='editar'
+            botaoEditar.onclick = () => editar(i)
+
+
+            tarefa.appendChild(botaoRemover)
+            tarefa.appendChild(botaoEditar)
+            lista.appendChild(tarefa)
+
+            localStorage.setItem('tarefas', JSON.stringify(tarefas))
+         }
+}
+
+function remover(i) {
+    tarefas.splice(i, 1)
+    renderizarTarefa()
+}
+
+function editar(i){
+   
+    botaoPrincipal.value = 'Alterar'
+    inserirTarefa.value = tarefas[i]
+    botaoPrincipal.onclick = () => alterar(i)
     
 }
- 
+
+function alterar(i){
+    tarefas[i] = (inserirTarefa.value)
+    renderizarTarefa()
+    botaoPrincipal.value = 'Adicionar Tarefa'
+    inserirTarefa.value=''
+    botaoPrincipal.onclick = () => adicionar()
+}
+
+function recuperarTarefas(){
+    tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+    renderizarTarefa()
+    console.log(localStorage.getItem('tarefas'))
+}
